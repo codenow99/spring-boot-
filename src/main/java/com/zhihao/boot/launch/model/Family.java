f08cff706f774d072d17ed5f43390d70ec9350d9
codenow99 <zhihaocheng1999@163.com>
2020-10-15 19:16:43 +0800

    third commit

--- src/main/java/com/zhihao/boot/launch/model/Family.java
+++ src/main/java/com/zhihao/boot/launch/model/Family.java
@@ -1,5 +1,6 @@
 package com.zhihao.boot.launch.model;
 
+import com.zhihao.boot.launch.service.MixPropertySourceFactory;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
@@ -12,7 +13,7 @@ import javax.validation.constraints.NotEmpty;
 @Component
 @Validated
 @ConfigurationProperties(prefix = "family")
-@PropertySource(value = "classpath:family.yml")
+@PropertySource(value = "classpath:family.yml", factory = MixPropertySourceFactory.class)
 public class Family {
 
     //@Value("${family.family-name}")
